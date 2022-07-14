c080b132decf4822020839d5f92af1cd06fccd12
adam.u <ubad.adam@gmail.com>
2022-07-14 21:10:21 +0800

    Add Activity Indicator

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus Arrival Time:</Text>
+      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={"blue"} />: "loaded"} </Text>
+      <TouchableOpacity style={styles.button} onPress={()=>setLoading(false)}>
+      <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +21,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title:{
+  fontSize:30,
+  marginVertical: 20,
+  },
+  arrivalTime:{
+    fontSize: 20,
+  },
+  button:{
+    backgroundColor: 'green',
+    padding: 20,
+    marginVertical: 20,
+  },
+  buttonText:{fontSize: 30,},
+
 });
